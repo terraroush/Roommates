578883fcb25448ea204306fa787312a468a39fa7
Terra Roush <terra.roush@gmail.com>
2020-12-03 15:43:38 -0600

    chore repository with three methods

---
+++ Roommates/Models/Chore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Roommates.Models
+{
+    public class Chore
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
