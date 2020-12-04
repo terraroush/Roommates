1799840c1d3173107769ca16e84f21577e50794c
Terra Roush <terra.roush@gmail.com>
2020-12-04 14:00:09 -0600

    refactored switch statement

--- Roommates/Repositories/RoommateRepository.cs
+++ Roommates/Repositories/RoommateRepository.cs
@@ -1,10 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Data.SqlClient;
+using Roommates.Models;
 
 namespace Roommates.Repositories
 {
-    class RoommateRepository
+    class RoommateRepository : BaseRepository
     {
+        public RoommateRepository GetById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT FirstName, LastName, RentPortion, r.Name as RoomName FROM Roommate rmt WHERE Id = @id JOIN Room r ON rmt.RoomId = r.id";
+                    cmd.Parameters.AddWithValue("@id", id);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Models.Roommate roommate = null;
+
+                    if (reader.Read())
+                    {
+                        roommate = new Roommate
+                        {
+                            Id = id,
+                            Firstname = reader.GetString(reader.GetOrdinal("FirstName")),
+                            Lastname = reader.GetString(reader.GetOrdinal("LastName")),
+                            RentPortion = reader.GetInt32(reader.GetOrdinal("RentPortion")),
+                            Room = reader.GetString(reader.GetOrdinal("Name")),
+                        };
+                    }
+                }
+            }
+        }
+        public RoommateRepository(string connectionString) : base(connectionString) { }
+
     }
 }
